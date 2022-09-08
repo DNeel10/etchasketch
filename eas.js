6523815a0c4bf94312cdaa7f8fe79600a6f13897
Danny Neel <dneel10@gmail.com>
2022-09-07 23:01:10 -0400

    add rainbow button functionality

--- eas.js
+++ eas.js
@@ -2,6 +2,7 @@ const gridContainer = document.querySelector("#gridContainer");
 const container = document.querySelector(".container");
 const rows = document.querySelectorAll(".row");
 const cell = document.querySelector(".cell");
+let rgb = ''
 
 
 // make grid by nesting for loops to get a number of rows, and within each row, creating the same number of cells
@@ -54,27 +55,14 @@ output.textContent = `${slider.value} x ${slider.value}`;
 // CREATE NEW GRID USING THE SIZE FROM THE INPUT SLIDER
 slider.onmouseup = function() {
     output.innerHTML=`${slider.value} x ${slider.value}`;
-    removeGrid();
-    makeGridNew(slider.value);
-
-    // this allows the reset button to accept slider.value (why is the slider.value logging every number that the slider has landed on in sequence)
-    resetGrid(slider.value);
-
-    //adding event listeners to the new grid
-    const cells = document.querySelectorAll('.cell');
-
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover',() => {
-            cell.style.backgroundColor='black';
-        });
-    });
+    resetGridWithout(slider.value);
+    defaultPen();
 }
 
 // update slider display as you move it
 slider.oninput = function() {
     output.innerHTML=`${slider.value} x ${slider.value}`;
 }
-
 
 // Remove existing grid on slider adjustment
 
@@ -86,7 +74,11 @@ function removeGrid(){
 }
 
 // BUTTONS:
+// set button variables
 const reset = document.getElementById("reset");
+const rainbow = document.getElementById("rainbow");
+const gradient = document.getElementById("gradient");
+const colorSelect = document.getElementById("colorSelect")
 
 // reset
 function resetGrid(sliderValue){
@@ -95,23 +87,62 @@ function resetGrid(sliderValue){
     defaultPen();
 }
 
-// color picker
-function pickColor(r, g, b) {
+reset.addEventListener('click', () => {resetGrid(slider.value)});
 
+// reset without default pen included
+function resetGridWithout (sliderValue){
+    removeGrid();
+    makeGridNew(sliderValue);
 }
+
+// Color Selector - To be implemented
+
+/*function pickColor(r, g, b) {
+    console.log("Pick a Color");
+}
+colorSelect.addEventListener('click', () => {pickColor()});
+*/
 
 // rainbow random color
-function getRandomColor(r, g, b) {
 
+function getRandomColor() {
+    r = getRandomIntInclusive(0, 255);
+    g = getRandomIntInclusive(0, 255);
+    b = getRandomIntInclusive(0, 255);
+    let rgb = `rgb(${r}, ${g}, ${b})`;
+    return rgb;
 }
+
+function getRandomIntInclusive(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min+1) + min);
+}
+
+function randomPen() {
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover',() => {
+            let color = getRandomColor();
+            cell.style.backgroundColor=color;
+        });
+    });
+}
+
+rainbow.addEventListener('click', () => {
+    resetGridWithout(slider.value);
+    randomPen();
+});
 
 // gradient pen
 function selectGradient(){
-
+    console.log("10% each pass");
 }
 
-reset.addEventListener('click', () => {resetGrid(slider.value)});
+gradient.addEventListener('click', () => {selectGradient()});
 
+// call initial functions to set up page
 makeGridInitial(64);
 defaultPen();
 resetGrid();
